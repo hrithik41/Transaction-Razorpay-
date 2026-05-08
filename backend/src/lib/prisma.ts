9c7829f61815df6bddcd0447918be7a853ce1728
Hrithik Mishra <littlehrithik9594@gmail.com>
2026-05-08 11:41:28 +0530

    Prisma.ts modified

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -7,10 +7,6 @@ const adapter = new PrismaMariaDb({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    port: Number(process.env.DATABASE_PORT),
-    ssl: {
-        rejectUnauthorized: false
-    }
 });
 
 const prisma = new PrismaClient({ adapter });
